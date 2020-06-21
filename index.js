37ebda5e8389bcae8b92b583f9d3f83e66adcd08
alaneurich <alan.eur@gmail.com>
2020-06-21 12:09:21 +0200

    ADD: Yarn Lock File

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
  * @format
  */
 
-import { Provider as PaperProvider } from 'react-native-paper';
+import {Provider as PaperProvider} from 'react-native-paper';
 import * as React from 'react';
-import { AppRegistry } from 'react-native';
+import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
